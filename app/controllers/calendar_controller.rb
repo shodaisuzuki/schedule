16446d0b83fa7ab97afcb6547cf04bb85a6c125c
shodai <shodai-connect@tbz.t-com.ne.jp>
2016-07-19 19:33:42 +0900

    Add toggle color

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -1,6 +1,25 @@
-class CalendarController < ApplicationController
+class CalendarsController < ApplicationController
 protect_from_forgery with: :exception
 	def index
-		
+	end
+
+	def show
+		@calendar = Calendar.find(params[:id])
+	end
+
+	def new
+		@calendar = Calendar.new
+	end
+
+	def create
+		@calendar = Calendar.new(calendar_params)
+		@calendar.save
+		redirect_to @calendar
+	end
+
+	private
+
+	def calendar_params
+		params.require(:calendar).permit(:name)
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
-  root 'calendar#index'
+  root 'calendars#index'
+
+  resources :calendars
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
